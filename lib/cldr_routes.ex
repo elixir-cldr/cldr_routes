ca562e794c4affa5950b75cb5a052195424d692c
Kip Cole <kipcole9@gmail.com>
2022-03-08 12:37:32 +0800

    Initial commit

---
+++ lib/cldr_routes.ex
@@ -0,0 +1,18 @@
+defmodule CldrRoutes do
+  @moduledoc """
+  Documentation for `CldrRoutes`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CldrRoutes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cldr_routes_test.exs
@@ -0,0 +1,8 @@
+defmodule CldrRoutesTest do
+  use ExUnit.Case
+  doctest CldrRoutes
+
+  test "greets the world" do
+    assert CldrRoutes.hello() == :world
+  end
+end
