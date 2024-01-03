a5b2720d05fb0ae4d3fce5e893670689df20d21a
Kip Cole <kipcole9@gmail.com>
2024-01-03 12:12:20 +0800

    Use PhoenixHTMLHelpers as a dependency

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CldrRoutes.MixProject do
   use Mix.Project
 
-  @version "1.1.0"
+  @version "1.2.0"
 
   def project do
     [
@@ -53,12 +53,12 @@ defmodule CldrRoutes.MixProject do
   defp deps do
     [
       {:ex_cldr, "~> 2.37"},
-      {:phoenix, "~> 1.7"},
+      {:phoenix_html_helpers, "~> 1.0"},
       {:phoenix_live_view, "~> 0.18", optional: true},
       {:jason, "~> 1.0"},
       {:gettext, "~> 0.19"},
       {:ex_doc, "~> 0.18", only: [:release, :dev]},
-      {:dialyxir, "~> 1.0", only: [:dev, :release], runtime: false, optional: true}
+      {:dialyxir, "~> 1.0", only: [:test, :dev, :release], runtime: false, optional: true}
     ]
   end
 
